a9408c1827809ce4b4d66b9d52d0950f353cf659
PrincessGrouchy <a.rebecca.reid@gmail.com>
2022-12-30 22:06:56 -0700

    strip dupes dif file

--- Current Solution/pokecenter_trawl_strip_dupes.py
+++ Current Solution/pokecenter_trawl_strip_dupes.py
@@ -4,14 +4,22 @@ from datetime import date
 
 
 class pokecenter_strip_dupes():
-    def setup_method(self, the_region):
+    def setup_method(self, the_region, the_date=date.today()):
         self.region = the_region
 
+        # self.current_file_name = 'pokecenter_output_' + self.region + '_current.txt'
         # self.current_file = pd.read_csv(
         #     'pokecenter_output_' + self.region + '_current.txt', encoding="utf-8",
         #     names=['relative_position', 'link', 'id', 'name',
         #            'price', 'in_stock', 'page_position', 'category']
         # )
+        self.diff_file_name = 'pokecenter_output_' + \
+            self.region + '_diff_{}.txt'.format(the_date)
+        self.diff_file = pd.read_csv(
+            self.diff_file_name, encoding="utf-8",
+            names=['relative_position', 'link', 'id', 'name', 'price',
+                   'in_stock', 'page_position', 'category', 'change']
+        )
         self.complete_file_name = 'pokecenter_output_' + self.region + '_complete.txt'
         self.complete_file = pd.read_csv(
             self.complete_file_name, encoding="utf-8", header=0,
@@ -19,18 +27,21 @@ class pokecenter_strip_dupes():
                    'in_stock', 'category', 'search_status']
         )
 
-    def deduplicate(self, file):
+    def deduplicate(self, file, filename):
         file = file.sort_values(by=["link"], ascending=True)
         file.drop_duplicates(subset=['link', 'id'], inplace=True)
-        file.to_csv(self.complete_file_name, index=False, header=False)
+        file.to_csv(filename, index=False, header=False)
 
     def run_dedupe(self):
-        # self.deduplicate(self.current_file)
-        self.deduplicate(self.complete_file)
+        # self.deduplicate(self.current_file, self.current_file_name)
+        self.deduplicate(self.diff_file, self.diff_file_name)
+        self.deduplicate(self.complete_file, self.complete_file_name)
 
 
 regions = ["en-ca", "en-gb", "en-us", "jp-jp"]
 reader = pokecenter_strip_dupes()
 for the_region in regions:
     reader.setup_method(the_region)
+    ## manual mode
+    # reader.setup_method(the_region, date(2023, 01, 17))
     reader.run_dedupe()
